43f9893fc47c3573af52bfad4e42384c1c9796c2
Ivlion <gleb.ivlev66@gmail.com>
2025-02-01 12:16:20 +0300

    some changes

--- statistic.py
+++ statistic.py
@@ -21,7 +21,7 @@ class Name(QWidget):
         self.btn.clicked.connect(self.get_name)
 
         self.label = QLabel(self)
-        self.label.setText("В имени можно использовать только\nсимволы латинского алфавита и символ '_'")
+        self.label.setText("В имени можно использовать только\nсимволы латинского алфавита,\nцифры и символ '_'")
         self.label.move(20, 50)
 
         self.name_label = QLabel(self)
@@ -33,9 +33,13 @@ class Name(QWidget):
 
     def get_name(self):
         name = self.name_input.text()
+        for el in name:
+            if el.lower() not in 'qwertyuiopasdfghjklzxcvbnm1234567890_':
+                return
         new_row = [name, str(self.c), str(self.t)]
         with open('save.csv', 'a', newline='', encoding='utf8') as f:
             csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONNUMERIC, quotechar='"').writerow(new_row)
+        self.close()
 
 
 def save(c, t):
@@ -54,14 +58,30 @@ def save(c, t):
 class Table(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(300, 300, 300, 300)
-        self.setFixedSize(300, 300)
+        self.setGeometry(300, 300, 300, 325)
+        self.setFixedSize(300, 325)
         self.setWindowTitle('Table')
-        self.tableWidget = QTableWidget(self)
-        self.loadTable('save.csv')
 
-    def loadTable(self, table_name):
-        with open(table_name, encoding="utf8") as csvfile:
+        self.i = None
+        self.table_name = 'save.csv'
+
+        self.btn = QPushButton('Удалить запись', self)
+        self.btn.resize(140, 20)
+        self.btn.move(5, 305)
+        self.btn.clicked.connect(self.delete_record)
+        self.btn.setEnabled(False)
+
+        self.btn1 = QPushButton('Удалить все записи', self)
+        self.btn1.resize(140, 20)
+        self.btn1.move(150, 305)
+        self.btn1.clicked.connect(self.delete_all)
+
+        self.tableWidget = QTableWidget(self)
+        self.loadTable()
+        self.tableWidget.cellClicked.connect(self.click)
+
+    def loadTable(self):
+        with open(self.table_name, encoding="utf8") as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='"')
             title = next(reader)
             self.tableWidget.setColumnCount(len(title))
@@ -76,6 +96,31 @@ class Table(QWidget):
                     self.tableWidget.setItem(i, j, QTableWidgetItem(elem))
         self.tableWidget.resizeColumnsToContents()
 
+    def click(self, i):
+        self.i = self.tableWidget.currentIndex().row()
+        self.btn.setEnabled(True)
+        print(self.i)
+
+    def delete_record(self):
+        if self.i != None:
+            with open(self.table_name, encoding="utf8", newline='') as f:
+                reader = csv.reader(f, delimiter=',', quotechar='"')
+                rows = [row for idx, row in enumerate(reader) if idx != self.i]
+                print(rows)
+            with open(self.table_name, 'w') as f:
+                writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONNUMERIC, quotechar='"', lineterminator='\r')
+                writer.writerows(rows)
+            self.loadTable()
+
+    def delete_all(self):
+        with open(self.table_name, encoding="utf8", newline='') as f:
+            reader = csv.reader(f, delimiter=',', quotechar='"')
+            row = list(reader)[0]
+        with open(self.table_name, 'w') as f:
+            writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONNUMERIC, quotechar='"', lineterminator='\r')
+            writer.writerow(row)
+        self.loadTable()
+
 
 def table():
     if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
@@ -88,4 +133,4 @@ def table():
     ex = Table()
     ex.show()
     app.exec()
-#table()
+table()
